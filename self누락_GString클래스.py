927dcb80cee2dfc901679ae01e179fdc73f0b33d
gohilady <gohilady1@gmail.com>
2022-04-19 13:44:12 +0900

    클래스 버그 수정

--- self누락_GString클래스.py
+++ self누락_GString클래스.py
@@ -1,11 +1,18 @@
 str = "Not Class Member"
+
+#클래스 정의
 class GString:
+    #초기화 메서드
     def __init__(self):
+        #인스턴스 멤버 변수
         self.str = "" 
+    #셋팅하는 메서드(세터)
     def set(self, msg):
         self.str = msg
+    #값을 읽는 게터
     def print(self):
-        print(str)
+        #버그 발생!
+        print(self.str)
 
 g = GString()
 g.set("First Message")
