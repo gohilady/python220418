1a99cb5fd8955bd606f33f859d907f59bd6b689b
gohilady <gohilady1@gmail.com>
2022-04-20 13:07:11 +0900

    파일 입출력

---
+++ DemoFile.py
@@ -0,0 +1,27 @@
+# DemoFile.py
+
+url = "http://www.credu.com/?page=" + str(1)
+print(url)
+
+for x in range(1,6):
+    print(x,"*",x,"=",x*x)
+
+print("---약간 변경---")
+
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).rjust(3))
+
+print("---약간 변경---")
+for x in range(1,6):
+    print(x,"*",x,"=",str(x*x).zfill(10))
+
+# 서식묹자를 지정
+print("{0:x}".format(10))
+print("{0:b}".format(10))
+
+
+#파일을 생성하기
+f = open("c:\\work\\demo.txt", "wt", encoding="utf-8")
+f.write("하나\\n두번째\n세번째\n")
+f.close()
+
---
+++ demoStr.py
@@ -0,0 +1,20 @@
+# demoStr.py
+
+print(dir(str))
+
+strA = "파이썬은 무지 강력해!"
+print(len(strA))
+strB = "python is very powerful"
+print(len(strB))
+print(strB.capitalize())
+print(strB.count("p"))
+print(strB.count("p",7))
+
+print("demo.ppt".endswith("ppt"))
+print("MBC2580".isalnum())
+print("MBC:2580".isalnum())
+print("2580".isdecimal())
+
+strC = "<<  spam and ham  >>"
+print(strC)
+print(strC.strip())
